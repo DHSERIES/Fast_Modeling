2a522700586313b723ef591a4627a8e2a6ed612b
DHSERIES <124233525+DHSERIES@users.noreply.github.com>
2025-05-06 09:57:30 +0000

    update EDA

--- EDA.py
+++ EDA.py
@@ -1,19 +1,13 @@
 # eda.py
 import streamlit as st
 import pandas as pd
-import sweetviz as sv
 import os
+from ydata_profiling import ProfileReport
+from streamlit_pandas_profiling import st_profile_report
 
 REPORT_DIR = "report/"
 
 @st.cache_data
-def make_report(df, report_dir):
-    os.makedirs(report_dir, exist_ok=True)
-    report = sv.analyze(df)
-    out = os.path.join(report_dir, "sweetviz_report.html")
-    report.show_html(out)
-    return out
-
 def show_eda():
     st.title("📊 EDA with Sweetviz")
 
@@ -25,8 +19,7 @@ def show_eda():
     df = pd.read_csv(path)
     st.write("Preview:", df.head())
 
-    if st.button("Generate Sweetviz report"):
-        report_path = make_report(df, REPORT_DIR)
-        with open(report_path) as f:
-            html = f.read()
-        st.components.v1.html(html, height=700, scrolling=True)
+    if st.button("Generate Profiling Report"):
+        with st.spinner("Generating..."):
+            pr = ProfileReport(df, explorative=True)
+            st_profile_report(pr)
